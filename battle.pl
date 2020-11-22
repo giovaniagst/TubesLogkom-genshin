d848dc90a1ea02b487e65513d6fe739f4560772b
rheael <68505778+rheael@users.noreply.github.com>
2020-11-22 23:47:37 +0700

    update

--- battle.pl
+++ battle.pl
@@ -1,5 +1,6 @@
 initiateBattle :-
-    inBattle(Enemy,Player). /*Kayak dibikin jadi true(?)*/
+    inbattle(_,Player,1). /*Ini kondisinya kalo emang udah ada musuh, dianggep inbattle*/
+    /*Kalo misal belakangnya 1, artinya lagi dalam battle*/
     % Ceritanya ada musuh yang muncul
 	write('List of things you are able to do:'),nl,
 	write('- attack.'),nl,
@@ -11,79 +12,86 @@ initiateBattle :-
 
 display_enemy_status :-
     /*Kita display kayak state enemy di sini*/
-    enemy_status(name,health,XP).
-    write(name),nl,
-    write(health),nl,
-    write(exp),nl.
+    enemy_status(Name,Health,XP).
+    write(Name),nl,
+    write(Health),nl,
+    write(Exp),nl.
+
+/*Variabel, ngikutin enemy.pl*/
+enemy_status(Name,Health,Exp).
 
 /*Variabel*/
-enemy_status(name,health,XP).
-
-/*Variabel*/
-player_status(name,health,XP).
+player_status(Name,Health,Exp).
 
 display_player_status :-
     /*Bikin state player di sini*/
-    player_status(name,health,XP),
-    write(name),nl,
-    write(health),nl,
-    write(exp),nl.
+    player_status(Name,Health,Exp),
+    write(Name),nl,
+    write(Health),nl,
+    write(Exp),nl.
 
 use_potion :-
-	check_amount_of_potion(X),
+	check_amount_of_potion(X), /*Asumsi udah didefinisiin*/
 	X=0, /*Gaada potion*/
-	write('No potion to use.').
+	write('No potion to use.'),!.
 
 use_potion :-
 	check_amount_of_potion(X), /*Asumsi udah didefinisiin*/
 	X/=0,
-	retract(statePlayer(Name,XP,Health)),
+	retract(player_status(Name,Health,Exp)),
 	potion(PotionName, AddHealth), /*Asumsinya semua potion di-state menambah health berapa*/
 	NewHealth is Health+AddHealth,
-	assert(statePlayer(Name,XP,Health)),
+	assert(player_status(Name,Health,Exp)),
 	write('Yay, your health improved by', ).
 
 /*Hubungan potionnya ntar sama yang disimpen*/
 
 attack :-
-	\+inbattle(), %Kalo misal ga lagi dalam pertarungan
+	inbattle(_,Player,0), %Kalo misal ga lagi dalam pertarungan
 	write('Command cannot be used because you are not in a battle.'),nl,!.
 
 attack :-
-    inbattle(_,Player,_,_),
-    /*Definisin attacknya satu2, ada damagenya*/
-    /*Atau dirandom aja damagenya*/
+    inbattle(_,Player,1),
     /*Berdasarkan inventory*/
-    write('Damage!!!')
-    retract(statePlayer(Name,XP,Health)).
-    Health is Health-Damage,
-    XP is XP-Damage, /*Masih ngasal banget yang pengurangannya*/
-    assert(statePlayer(Name,XP,Health)).
+    write('Damage!!!'),
+    retract(player_status(Name,Health,Exp)),
+    NewHealth is Health-Damage,
+    NewXP is XP-Damage, /*Masih ngasal banget yang pengurangannya*/
+    assert(player_status(Name,Health,Exp)).
+
+useInventory :-
+	check_amount_of_inventory(X),
+	X=0, /*Gaada inventory*/
+	write('No inventory to use.'),!.
+	
+useInventory :-
+	check_amount_of_inventory(X),
+	X/=0,
+	chooseInventory.
+
+chooseInventory :-
+	/*Gatau belum ngerti cara milih barang di inventory*/
 
 special_attack:
     /*Sama kayak attack, tapi damagenya lebih gede*/
 
 win:-
-    checklose(BossEnemy),
+    checkwin,
+    retract(inbattle(_,Player,1),
     write('YOU WON!'),nl,
+    assert(inbattle(_,Player,0). /*Dibikin jadi 0 artinya dia keluar dari battle*/
     /*write....*/
 
 lose :-
-    checklose(Player).
+    checklose(IDName).
     write('YOU LOSEEEEEEE!'),nl.
     /*write...*/
 
-checklose(Name):-
-    statePlayer(Name,0,Health).
+checklose:-
+    statePlayer(IDName,0,_).
 
-modify(Name):-
-    retract(statePlayer(Name,_,_).
-    XP is XP*(),
-    Health is Health+(),
-    assert(statePlayer(Name,XP,Health)).
-
-check_boss_enemy:-
-    stateEnemy(BossEnemy,_,_).
+checkwin :-
+    stateEnemy(BossEnemy,0,_).
 
 enemy_attack:-
 
@@ -98,13 +106,12 @@ runfail:-
     write('Bad luck! You are not able to run~').
 
 runsuccess:-
-    write('You successfully run from the enemy.')
+    retract(inbattle(_,Player,1),
+    write('You successfully run from the enemy.'),
+    assert(inbattle(_,Player,0). /*Dibikin jadi 0 artinya dia keluar dari battle*/
 
 /*Inget selalu dicek lagi inBattle apa ngga*/
 /*Cek yang boss enemy, kalo udah selesai ya menang*/
-/*Dirandom runnya bisa atau ngga*/
-/*Bisa jadi bikin fungsi baru untuk ngemodify-health sama damage-nya*/
-/*Pemanfaatan inventorynya gimana*/
 /*Ada bagian turn musuh*/
 /*Ngecek enemy dead*/
 /*Damagenya berdasarkan inventory*/
