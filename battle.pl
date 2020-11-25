2dc33bb03d80e1b796e46b5ffe60bb7a5cb68707
rheael <68505778+rheael@users.noreply.github.com>
2020-11-25 14:23:53 +0700

    Update battle.pl

--- battle.pl
+++ battle.pl
@@ -6,12 +6,12 @@
 :- dynamic(enemyfound/1).
 
 start:- /*Ini buat back-up sebelum nanti dihubungin sama file lain*/
-	asserta(player_status(id,3000,3000)),
+	asserta(player_status(id,10000,50000)),
 	enemyfound(someone).
 
 enemyfound(EnemyName):-
 	asserta(amountofenemy(1)),
-	asserta(enemy_status(EnemyName,5000,3000)),
+	asserta(enemy_status(EnemyName,8000,10000)),
 	write('Fight like a knight, or run like a coward. Your choice.'),nl,
 	write('Choose.'), nl,
 	write('- startfight.'), nl,
@@ -31,7 +31,7 @@ runluck(X):-
 runluck(X):-
     X = 0,
     write('Bad luck! You are not able to run~'),
-    fightstart.
+    startfight.
 
 startfight :-
     asserta(inbattle(_,_,1)),
@@ -52,11 +52,6 @@ fight :-
 attack :-
 	inbattle(_,_,0), /*Kalo misal ga lagi dalam pertarungan*/
 	write('Command cannot be used because you are not in a battle.'),nl,!.
-	
-attack :-
-   inbattle(_,_,1),
-   playerturn(0),
-   write('Not your turn yet!!'),!.
 
 attack :-
     inbattle(_,_,1),
@@ -64,6 +59,17 @@ attack :-
     write('Damage!!!'),
     retract(enemy_status(Name,Health,Exp)),
     random(1300,1500,Dmg),
+    NewHealth is Health-Dmg,
+    NewExp is Exp-Dmg,
+    asserta(enemy_status(Name,NewHealth,NewExp)),
+    check_enemydead.
+
+specialattack :-
+    inbattle(_,_,1),
+    playerturn(1),
+    write('Damage!!!'),nl,
+    retract(enemy_status(Name,Health,Exp)),
+    random(1700,2000,Dmg),
     NewHealth is Health-Dmg,
     NewExp is Exp-Dmg,
     asserta(enemy_status(Name,NewHealth,NewExp)),
@@ -102,6 +108,9 @@ check_playerdead :-
 
 whichTurn :- /*Bisa aja yang enemy yang mau attack*/
         write('Wait for your luck...'),nl,
+	write('...'),nl,
+	write('...'),nl,
+	write('...'),nl,
 	random(0,2,X),
         decide(X).
 
@@ -118,68 +127,3 @@ decide(X) :-
 	asserta(playerturn(1)),
 	write('Your turn!'),nl,
 	fight.
-
-
-/* ##################################### */
-
-/* INI BAWAH BELUMMMMM */
-
-/* SPECIAL ATTACK JUGA BELUM */
-
-/* DAMAGE BERDASARKAN INVENTORY BUKAN RANDOM */
-
-/* Perlevelan gimana ya */
-
-/* Kayaknya ntar bakal pake halt di akhir */
-
-/* Mau cari tau arti reset apaan */
-
-% use_potion :-
-%	check_amount_of_potion(X), /*Asumsi udah didefinisiin*/
-%	X=0, /*Gaada potion*/
-%	write('No potion to use.'),!.
-
-% use_potion :-
-%	check_amount_of_potion(X),
-%	X/=0,
-%	player_status(Name,Health,Exp),
-%	Health==MaxHealth, /*Istilahnya udah ga bisa nambah lagi healthnya(?), ga penting sih*/
-%	write('You're fully healthy~ what is potion for?').
-
-% use_potion :-
-%	check_amount_of_potion(X), /*Asumsi udah didefinisiin*/
-%	X/=0,
-%	player_status(Name,Health,Exp),
-%	Health/=MaxHealth,
-%	retract(player_status(Name,Health,Exp)),
-%	potion(PotionName, AddHealth), /*Asumsinya semua potion di-state menambah health berapa*/
-%	NewHealth is Health+AddHealth,
-%	assert(player_status(Name,NewHealth,Exp)),
-%	write('Yay, your health improved by', ).
-%
-% /*Hubungan potionnya ntar sama yang disimpen*/
-
-% useInventory :-
-%	check_amount_of_inventory(X),
-%	X==0, /*Gaada inventory*/
-%	write('No inventory to use.'),!.
-%	
-% useInventory :-
-%	check_amount_of_inventory(X),
-%	X/=0,
-%	chooseInventory.
-%
-% chooseInventory :-
-%	/*Gatau belum ngerti cara milih barang di inventory*/
-%
-% win:-
-%    checkwin,
-%    retract(inbattle(_,Player,1),
-%    write('YOU WON!'),nl,
-%    assert(inbattle(_,Player,0). /*Dibikin jadi 0 artinya dia keluar dari battle*/
-%    /*write....*/
-%
-% lose :-
-%    checklose(IDName).
-%    write('YOU LOSEEEEEEE!'),nl.
-%    /*write...*/
