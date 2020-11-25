ebddff782a40542e55d2473c13b737a707026fcd
rheael <68505778+rheael@users.noreply.github.com>
2020-11-25 14:25:35 +0700

    Update battle.pl

--- battle.pl
+++ battle.pl
@@ -127,3 +127,57 @@ decide(X) :-
 	asserta(playerturn(1)),
 	write('Your turn!'),nl,
 	fight.
+
+/* INI BAWAH BELUMMMMM */
+
+/* SPECIAL ATTACK JUGA BELUM */
+/* DAMAGE BERDASARKAN INVENTORY BUKAN RANDOM */
+/* Perlevelan gimana ya */
+/* Kayaknya ntar bakal pake halt di akhir */
+/* Mau cari tau arti reset apaan */
+% use_potion :-
+%	check_amount_of_potion(X), /*Asumsi udah didefinisiin*/
+%	X=0, /*Gaada potion*/
+%	write('No potion to use.'),!.
+% use_potion :-
+%	check_amount_of_potion(X),
+%	X/=0,
+%	player_status(Name,Health,Exp),
+%	Health==MaxHealth, /*Istilahnya udah ga bisa nambah lagi healthnya(?), ga penting sih*/
+%	write('You're fully healthy~ what is potion for?').
+% use_potion :-
+%	check_amount_of_potion(X), /*Asumsi udah didefinisiin*/
+%	X/=0,
+%	player_status(Name,Health,Exp),
+%	Health/=MaxHealth,
+%	retract(player_status(Name,Health,Exp)),
+%	potion(PotionName, AddHealth), /*Asumsinya semua potion di-state menambah health berapa*/
+%	NewHealth is Health+AddHealth,
+%	assert(player_status(Name,NewHealth,Exp)),
+%	write('Yay, your health improved by', ).
+%
+% /*Hubungan potionnya ntar sama yang disimpen*/
+% useInventory :-
+%	check_amount_of_inventory(X),
+%	X==0, /*Gaada inventory*/
+%	write('No inventory to use.'),!.
+%	
+% useInventory :-
+%	check_amount_of_inventory(X),
+%	X/=0,
+%	chooseInventory.
+%
+% chooseInventory :-
+%	/*Gatau belum ngerti cara milih barang di inventory*/
+%
+% win:-
+%    checkwin,
+%    retract(inbattle(_,Player,1),
+%    write('YOU WON!'),nl,
+%    assert(inbattle(_,Player,0). /*Dibikin jadi 0 artinya dia keluar dari battle*/
+%    /*write....*/
+%
+% lose :-
+%    checklose(IDName).
+%    write('YOU LOSEEEEEEE!'),nl.
+%    /*write...*/
