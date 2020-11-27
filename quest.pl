4adefb3a30f6564ec052f258ab47a456d23ac420
rheael <68505778+rheael@users.noreply.github.com>
2020-11-27 17:20:47 +0700

    coba dulu

--- quest.pl
+++ quest.pl
@@ -6,6 +6,10 @@ take_q :-,
  random(3,5,Y),
  random(3,6,Z),
  asserta(takequest(X,Y,Z)),
- write('You got'),write(X),write('slime'),
- write('You got'),write(Y),write('goblin'),nl,
- write('You got'),write(Z),write('wolf'),nl,!.
+ write('You got'),
+ write(X),
+ write(' slime '),nl,
+ write(Y),
+ write(' goblin, '),nl,
+ write(Z),
+ write(' wolf.'),nl,!.
