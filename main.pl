c65e9b4569b79cb42cc0212d9636d6b111a72648
giovaniagst <68476711+giovaniagst@users.noreply.github.com>
2020-11-27 19:49:30 +0700

    Update main.pl

--- main.pl
+++ main.pl
@@ -25,18 +25,20 @@ start:-
     write('Lets play the game!'), nl,
     write(''), nl,
     write(''), nl,
-    write('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%'), nl,
-    write('%                              GENSHIN ADVENTURE                               %'), nl,
-    write('% 1. start  : untuk memulai permainan                                          %'), nl,
-    write('% 2. map    : untuk menampilkan peta                                           %'), nl,
-    write('% 3. status : untuk menampilkan kondisi terkini pemain                         %'), nl,
-    write('% 4. w      : gerak ke atas sebanyak 1 langkah                                 %'), nl,
-    write('% 5. s      : gerak ke bawah sebnayak 1 langkah                                %'), nl,
-    write('% 6. a      : gerak ke kiri sebanyak 1 langkah                                 %'), nl,
-    write('% 7. d      : gerak ke kanan sebanyak 1 langkah                                %'), nl,
-    write('% 8. Status : menampilkan status pemain                                        %'), nl,
-    write('% 9. help   : menampilkan bantuan                                              %'), nl,
-    write('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%'), nl.
+    write('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%'), nl,
+    write('%                              GENSHIN ADVENTURE                                   %'), nl,
+    write('% 1.  start     : untuk memulai permainan                                          %'), nl,
+    write('% 2.  map       : untuk menampilkan peta                                           %'), nl,
+    write('% 3.  status    : untuk menampilkan kondisi terkini pemain                         %'), nl,
+    write('% 4.  w         : gerak ke atas sebanyak 1 langkah                                 %'), nl,
+    write('% 5.  s         : gerak ke bawah sebnayak 1 langkah                                %'), nl,
+    write('% 6.  a         : gerak ke kiri sebanyak 1 langkah                                 %'), nl,
+    write('% 7.  d         : gerak ke kanan sebanyak 1 langkah                                %'), nl,
+    write('% 8.  shop      : untuk masuk ke shop                                              %'), nl,
+    write('% 9.  help      : menampilkan bantuan                                              %'), nl,
+    write('% 10. inventory : menampilkan inventory pemain                                     %'), nl,
+    write('% 11. quit      : untuk keluar dari permainan                                      %'), nl,
+    write('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%'), nl.
 
 start:-
     choose_character,
@@ -46,18 +48,20 @@ quit :-
     halt, !.
 
 help :-
-    write('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%'), nl,
-    write('%                              GENSHIN ADVENTURE                               %'), nl,
-    write('% 1. start  : untuk memulai permainan                                          %'), nl,
-    write('% 2. map    : untuk menampilkan peta                                           %'), nl,
-    write('% 3. status : untuk menampilkan kondisi terkini pemain                         %'), nl,
-    write('% 4. w      : gerak ke atas sebanyak 1 langkah                                 %'), nl,
-    write('% 5. s      : gerak ke bawah sebnayak 1 langkah                                %'), nl,
-    write('% 6. a      : gerak ke kiri sebanyak 1 langkah                                 %'), nl,
-    write('% 7. d      : gerak ke kanan sebanyak 1 langkah                                %'), nl,
-    write('% 8. Status : menampilkan status pemain                                        %'), nl,
-    write('% 9. help   : menampilkan bantuan                                              %'), nl,
-    write('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%'), nl.
+    write('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%'), nl,
+    write('%                              GENSHIN ADVENTURE                                   %'), nl,
+    write('% 1.  start     : untuk memulai permainan                                          %'), nl,
+    write('% 2.  map       : untuk menampilkan peta                                           %'), nl,
+    write('% 3.  status    : untuk menampilkan kondisi terkini pemain                         %'), nl,
+    write('% 4.  w         : gerak ke atas sebanyak 1 langkah                                 %'), nl,
+    write('% 5.  s         : gerak ke bawah sebnayak 1 langkah                                %'), nl,
+    write('% 6.  a         : gerak ke kiri sebanyak 1 langkah                                 %'), nl,
+    write('% 7.  d         : gerak ke kanan sebanyak 1 langkah                                %'), nl,
+    write('% 8.  shop      : untuk masuk ke shop                                              %'), nl,
+    write('% 9.  help      : menampilkan bantuan                                              %'), nl,
+    write('% 10. inventory : menampilkan inventory pemain                                     %'), nl,
+    write('% 11. quit      : untuk keluar dari permainan                                      %'), nl,
+    write('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%'), nl.
 
 inventory :-
     show_inventory(X).
